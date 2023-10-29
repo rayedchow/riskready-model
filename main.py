3bc12a8c73190f6de7ac7367b841ea5b476f585a
voomp <bat7gamer7@gmail.com>
2023-10-29 15:02:51 -0400

    using cors

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
 from model import predictRisk
 from pandas import DataFrame
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['POST'])
+@cross_origin()
 def risk():
     data = request.get_json()
     predictedRisk = predictRisk(DataFrame(data))
